9fc95ab2a2ad363b6eec35dddc829c006e55c53f
IsaacS <greenace14@gmail.com>
2020-09-21 15:39:25 +0700

    +Add app will analyze your machine

--- app.js
+++ app.js
@@ -5,5 +5,22 @@
 // or
 // 'node app.js'
 // into the command line
+const os = require("os");
 
 console.log("Hello world");
+var operatingSystem = os.arch();
+console.log(
+  `Hmm it looks like the operating system I'm in is: ${operatingSystem}`
+);
+
+var userCpus = os.cpus();
+
+if (typeof userCpus.model !== "undefined") {
+  console.log(`the processor I can think with is: ${userCpus.model}`);
+}
+if (typeof userCpus.speed !== "undefined") {
+  console.log(`the processor is: ${userCpus.speed}`);
+}
+console.log(`it looks like I can use these devices to talk on...`);
+console.log(os.networkInterfaces());
+//os.networkInterfaces()
