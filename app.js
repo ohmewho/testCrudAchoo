564accc1d8ef967ca020bfc5b1d82fb36d2734bb
IsaacS <greenace14@gmail.com>
2020-09-21 15:25:32 +0700

    +Add HelloWorld
    'npm run app' or 'node app.js'

---
+++ app.js
@@ -0,0 +1,9 @@
+// to run the app
+// get your command line program to the repo's directory
+// then put
+// 'npm run app'
+// or
+// 'node app.js'
+// into the command line
+
+console.log("Hello world");
